78fa738d41249f8ea10679fd809174d128d15550
zanjs <root@zanjs.com>
2017-03-09 16:50:45 +0800

    Winners do what losers don't want to do :alien:

--- 0309/src/js/main.js
+++ 0309/src/js/main.js
@@ -49,8 +49,8 @@
      });    
       
       
-      $("body").on("click",".close-layer",function(){
-        PL.closeAll();
-      })
+    //   $("body").on("click",".close-layer",function(){
+    //     PL.closeAll();
+    //   })
 
 })();
