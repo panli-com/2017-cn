c945841e9dcde9b694d501ff8e6c9e42749dd641
zanjs <root@zanjs.com>
2017-03-09 16:51:40 +0800

    Proper preparation solves 80 percent of life's problems :horse:

--- 0309/src/js/main.js
+++ 0309/src/js/main.js
@@ -2,33 +2,7 @@
 
     $(function() {
 
-        // $(".floor-nav").on("click","li" ,function() {
-        //     var _t = $(this),
-        //         _tf = _t.attr("floor");
-        //         goTofloorC(_tf);
-        // });
-
-
-
-        // $(".back-top").on("click", function() {
-        //     $('body,html').animate({ scrollTop: 0 }, 300);
-        // });
-
-        // floorNnav();
-        // listData()
-
-    });
     
-
-    // $(window).resize(function() {
-    //     floorNnav();
-    // })
-
-
-    // $(window).scroll(function() {
-    //     var scrollTop = $(window).scrollTop();
-    //     $('.floor-nav')[scrollTop > 400 ? 'show' : 'hide']();
-    // });
 
      $(".sp-main").on("click",".click-a",function(){              
                  var _t = $(this),
@@ -46,11 +20,10 @@
                   skin: 'layui-Pan-nobg', //没有背景色
                   content: html
               });
-     });    
+     });
+     
       
-      
-    //   $("body").on("click",".close-layer",function(){
-    //     PL.closeAll();
-    //   })
+    })
+ 
 
 })();
