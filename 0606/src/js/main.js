c9c8297c4aa5289fd34f7b4e7c37a02ef7d65925
zanjs <root@zanjs.com>
2017-06-06 15:27:28 +0800

    樱花-专题-扫码 :anguished:

---
+++ 0606/src/js/main.js
@@ -0,0 +1,45 @@
+;
+(function() {
+
+  $(function() {
+
+    $(".floor-nav").on("click", "li", function() {
+      var _t = $(this),
+        _tf = _t.attr("floor");
+      goTofloorC(_tf);
+    });
+
+
+
+    $(".back-top").on("click", function() {
+      $('body,html').animate({ scrollTop: 0 }, 300);
+    });
+
+    $("#sp-close-tip").on("click", function() {
+      $(".sp-tip").hide()
+    })
+
+    floorNnav();
+    // listData()
+
+  });
+
+
+
+  $(window).resize(function() {
+    floorNnav();
+  })
+
+  $(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    var scrollTop2 = scrollTop;
+    var footerTop = $(".footer").offset().top - 700;
+
+    if (scrollTop > 400 && scrollTop2 < footerTop) {
+      $('.floor-nav').show()
+    } else {
+      $('.floor-nav').hide()
+    }
+  });
+
+})();
